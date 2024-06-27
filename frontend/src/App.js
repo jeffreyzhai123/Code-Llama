4bee52ecb323e23f41a28ff97eff771b1a0b9480
Jeffrey <jzhai08@students.cs.ubc.ca>
2024-06-26 21:03:22 -0700

    configured clerk for login authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import Home from './pages/home';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* use BrowserRouter to enable client side routing */}
+      <BrowserRouter>
+        <Routes>
+
+          {/* if url path is "/" render the Home component */}
+          <Route path="/" element={<Home/>}/>
+
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
