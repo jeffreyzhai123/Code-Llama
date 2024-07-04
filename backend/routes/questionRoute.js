816881f2d184cd27e8403c9cf87de1e9279a8339
zyning18 <zyning18@student.ubc.ca>
2024-07-04 13:51:35 -0600

    haha

---
+++ backend/routes/questionRoute.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const router = express.Router();
+import QuestionModel from "../models/QuestionModel.js"
+import db from "../config/db_connection.js"
+
+router.get('/questions', async (req, res) => {
+    let quesCollection = await db.collection("problembank");
+    let questionItems = quesCollection.find().toArray({});
+    console.log(questionItems);
+});
+
+router.get('/questions/:id', async (req, res) => {
+    let quesID = req.params.id;
+    let question = await db.collection("problembank");
+});
+
+export default router;
