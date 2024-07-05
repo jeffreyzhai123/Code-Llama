9ae632bc6ee06e86f39d10fa5e3f8a359da86e14
zyning18 <zyning18@student.ubc.ca>
2024-07-04 21:20:44 -0600

    There is something different in the db.connection file.

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,6 @@ app.use(express.json()); //middleware used to parse incoming JSON requests
 
 app.use('/answer', answerRouter);
 app.use('/question', questionRouter);
-/* await dbClient.connect();
-console.log("Connected to MongoDB Atlas! Let's fetch data");
-let problembank = dbClient.db('problembank');
-let easyQuestions = problembank.collection('easy');
-let qs = await easyQuestions.find().toArray();
-
-console.log(qs); */
-
-/* app.get('/', (req, res) => {
-    res.send("Hello World");
-}) */
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
