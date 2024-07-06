7ad988a405ef49e97cd20f8a8d2df810ef804810
HejiaQ <q9month@163.com>
2024-07-06 11:57:17 -0700

    Enable going back from the quiz result page + yaml update + dockerfile for frontend update

--- frontend/src/pages/quizResult.js
+++ frontend/src/pages/quizResult.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const QuizResult =()=>{
 
-    return <h>hiii</h>
+    return (
+    <div> 
+        <h>You have completed the quiz! Congrats!</h> 
+        <br></br>
+        <Link to="/">
+        <button className='backtoMain'> Go to the Main Page </button>
+        </Link>
+        
+    </div>
+    )
 }
 
 export default QuizResult
