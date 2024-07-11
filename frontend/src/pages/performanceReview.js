7f2c874df72b5abc9171351aa1773eed28406185
emmahy-park <emmapk@students.cs.ubc.ca>
2024-07-11 10:53:04 -0700

    edited performancereview_dev

--- frontend/src/pages/performanceReview.js
+++ frontend/src/pages/performanceReview.js
@@ -1,27 +1,81 @@
 // Display performance review
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
+import { QuizContext } from '../context/QuizResultContext';
+import { useUser } from '@clerk/clerk-react';
 
 const PerformanceReview = () => {
 
     const navigate = useNavigate();
     const mainButton = () => {
         navigate("/");
-    } 
-    const backButton = () => {
-        navigate("/performanceReview");
-    }
+    };
 
+    // const { sharedResult } = useContext(QuizContext);
+    const [scores, setScores] = useState("");
+    const {user} = useUser();
+    let user_id = user.id;
+    // let results = sharedResult;
+    // results.forEach((result) => {
+    //     let user_id = user.id;
+    //     let pass = result.passfail;
+    //     console.log(user_id);
+    // });
+    let score = 0;
+    let results = [];
+    let temporaryArray = [];
+    const fetchResults = async () => {
+        try {
+            const response = await fetch(`http:///localhost:3080/results/${user_id}`);
+            if (response.ok) {
+                const data = await response.json();
+                if (data.userid == user.id) {
+                    data.results.forEach((array, index) => {
+                        array.forEach((quiz, quizIndex) => {
+                            let pass = quiz.passfail;
+                            if (pass === true) score++;
+                        })
+                        let date = index + 1;
+                        let result = score;
+                        temporaryArray.push({date,result});
+                        score = 0;
+                    })  
+                }
+                //console.log(temporaryArray);
+                return temporaryArray;
+            }
+            
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+    };
+    
+    let scoreArray = fetchResults();
+
+    useEffect(() => {
+        scoreArray.then(result => {
+            setScores(result);
+        })
+    }, [scoreArray]);
+
+    //let scorefinal;
+    // console.log(scoreArray);
+    // scoreArray.then(result =>{
+    //     console.log(result);
+    // })
+
+    // const final = scoreArray;
+    //console.log(final);
     //TODO: GET SCORES
-    const scores = [
-        { date: "2024-07-01", time: "8:30", score: "56%", 
-            details: [{question: "Add two integers", passfail: "Fail"}]},
-        { date: "2024-07-01", time: "14:30", score: "80%",
-            details: [{question: "Multiply two integers", passfail: "Fail"}]},
-        { date: "2024-07-02", time: "14:30", score: "100%",
-            details: [{question: "Find the difference of two integers", passfail: "PASS"}]},
-    ];
-
+    // const scores = [
+    //     { date: "2024-07-01", time: "8:30", score: results, 
+    //         details: [{question: "Add two integers", passfail: "Fail"}]},
+    //     { date: "2024-07-01", time: "14:30", score: "80%",
+    //         details: [{question: "Multiply two integers", passfail: "Fail"}]},
+    //     { date: "2024-07-02", time: "14:30", score: "100%",
+    //         details: [{question: "Find the difference of two integers", passfail: "PASS"}]},
+    // ];
+    //console.log(scores);
     const[selectedScore, setSelectedScore] = useState(null);
     const[scoreboardVisible, setScoreboardVisible] = useState(true);
 
@@ -47,8 +101,7 @@ const PerformanceReview = () => {
                     <table>
                         <thead>
                             <tr>
-                                <th>Date</th>
-                                <th>Time</th>
+                                <th>Date & Time</th>
                                 <th>Score</th>
                             </tr>
                         </thead>
@@ -57,7 +110,6 @@ const PerformanceReview = () => {
                             {scores.map((score, index) => (
                                 <tr key={index} onClick={() => handleRowClick(score)} className="clickable-row">
                                     <td>{score.date}</td>
-                                    <td>{score.time}</td>
                                     <td>{score.score}</td>
                                 </tr>
                             ))}
