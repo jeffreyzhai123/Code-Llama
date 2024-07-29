291bb5486c2fd8b75184139a437f89435ad5761b
HejiaQ <q9month@163.com>
2024-07-29 09:36:31 -0700

    fixe the cancel option for admin mode, added more tests

---
+++ test/unitTest/calculatetest.js
@@ -0,0 +1,310 @@
+
+// const SAMPLE_USERS_ARRAY = [SAMPLE_USER_1, SAMPLE_USER_2]
+// const SAMPLE_LOQ = [SAMPLE_QUIZ_0, SAMPLE_QUIZ_100];
+// const SAMPLE_LOQ_2 = [SAMPLE_QUIZ_0];
+// const SAMPLE_LOQ_3 = [SAMPLE_QUIZ_100]
+
+const SAMPLE_QUIZ_1 = [
+    {
+        questionNum:1, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:2, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:3, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:4, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:5, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:6, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:7, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:8, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    }
+];
+const SAMPLE_QUIZ_2 = [
+    {
+        questionNum:1,
+        question:`function TestFunction(a,b) {return a + b}`,
+        answer:"",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1,
+        startTime:"7/26/2024, 4:15:35 PM",
+        endTime: "7/26/2024, 4:15:49 PM",
+        difficultyLevel:"Easy",
+        generatedCode:"No generated code since you did not provide an answer",
+        failedTestCases:"All tests failed since you did not provide an answer"
+    },
+    {
+        questionNum:2,
+        question:`function TestFunction(a,b) {return a - b}`,
+        answer:"",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1,
+        startTime:"7/26/2024, 4:15:35 PM",
+        endTime: "7/26/2024, 4:15:49 PM",
+        difficultyLevel:"Easy",
+        generatedCode:"No generated code since you did not provide an answer",
+        failedTestCases:"All tests failed since you did not provide an answer"
+    },
+    {
+        questionNum:3,
+        question:`function TestFunction(a,b) {return a * b}`,
+        answer:"",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1,
+        startTime:"7/26/2024, 4:15:35 PM",
+        endTime: "7/26/2024, 4:15:49 PM",
+        difficultyLevel:"Easy",
+        generatedCode:"No generated code since you did not provide an answer",
+        failedTestCases:"All tests failed since you did not provide an answer"
+    },
+    {
+        questionNum:4, 
+        question:`function TestFunction(a,b) {return a / b}`,
+        answer:"",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1,
+        startTime:"7/26/2024, 4:15:35 PM",
+        endTime: "7/26/2024, 4:15:49 PM",
+        difficultyLevel:"Easy",
+        generatedCode:"No generated code since you did not provide an answer",
+        failedTestCases:"All tests failed since you did not provide an answer"
+    },
+    {
+        questionNum:5, 
+        question:`function TestFunction(n)  {return n % 2 === 0;}`,
+        answer:"",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1,
+        startTime:"7/26/2024, 4:15:35 PM",
+        endTime: "7/26/2024, 4:15:50 PM",
+        difficultyLevel:"Easy",
+        generatedCode:"No generated code since you did not provide an answer",
+        failedTestCases:"All tests failed since you did not provide an answer"
+    },
+    {
+        questionNum:6, 
+        question:`function TestFunction(n)  {return n % 2 === 1;}`,
+        answer:"",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1,
+        startTime:"7/26/2024, 4:15:35 PM",
+        endTime: "7/26/2024, 4:15:50 PM",
+        difficultyLevel:"Easy",
+        generatedCode:"No generated code since you did not provide an answer",
+        failedTestCases:"All tests failed since you did not provide an answer"
+    },
+    {
+        questionNum:7, 
+        question:`function TestFunction(s)  {return s.length;}`,
+        answer:"",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1,
+        startTime:"7/26/2024, 4:15:35 PM",
+        endTime: "7/26/2024, 4:15:50 PM",
+        difficultyLevel:"Easy",
+        generatedCode:"No generated code since you did not provide an answer",
+        failedTestCases:"All tests failed since you did not provide an answer"
+    },
+    {
+        questionNum:8, 
+        question:`function TestFunction(n)  {return Math.pow(n, 2);}`,
+        answer:"",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1,
+        startTime:"7/26/2024, 4:15:35 PM",
+        endTime: "7/26/2024, 4:15:50 PM",
+        difficultyLevel:"Easy",
+        generatedCode:"No generated code since you did not provide an answer",
+        failedTestCases:"All tests failed since you did not provide an answer"
+    },
+];
+
+const SAMPLE_QUIZ_3 =  [
+    {
+        questionNum:1, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:2, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:3, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:4, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:5, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that times two numbers together",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1
+    },
+    {
+        questionNum:5, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:2
+    },
+    {
+        questionNum:6, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that add two numbers together",
+        reasonofchange:"",
+        passfail:true,
+        attemptNum:1
+    },
+    {
+        questionNum:7, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that subtract two numbers together",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1
+    },
+    {
+        questionNum:7, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that subtract two numbers together",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:2
+    },
+    {
+        questionNum:8, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that minus two numbers together",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:1
+    },
+    {
+        questionNum:8, 
+        question: "def foo(a,b): return a + b",
+        answer: "generate a function that minus two numbers together",
+        reasonofchange:"",
+        passfail:false,
+        attemptNum:2
+    }
+];
+
+
+describe("calculate score", ()=>{
+    it("calculate all correct", ()=>{
+        let score = calculateScore(SAMPLE_QUIZ_1);
+        expect(score)
+        .to
+        .equal(1);
+    });
+    
+    it("calculate all incorrect", ()=>{
+        let score = calculateScore(SAMPLE_QUIZ_2);
+        expect(score)
+        .to
+        .equal(0);
+    });
+
+    it("calculate two are incorrect and success at different attempts", ()=>{
+        let score = calculateScore(SAMPLE_QUIZ_3);
+        expect(score)
+        .to
+        .equal(6/8);
+    }); 
+
+});
+
+
+describe("calculate scaled score", ()=>{
+    it("calculate all correct", ()=>{
+
+    });
+    
+    it("calculate all incorrect", ()=>{
+        
+    });
+
+    it("calculate two are incorrect", ()=>{
+        
+    }); 
+
+});
